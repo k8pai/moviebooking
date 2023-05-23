2cddf0053944909adc67a0b86b9497dce4116221
k8pai <thek8pai@gmail.com>
2023-05-23 23:50:52 +0530

    removed unwanted imports

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,16 +1,12 @@
 const {
-	getMovies,
 	findAllMovies,
 	findOne,
 	findShows,
-	findPublished,
-	findReleased,
 } = require('../controllers/movie.controller');
 const movieRouter = require('express').Router();
 
 movieRouter.get('/movies', findAllMovies);
 movieRouter.get('/movies/:id', findOne);
 movieRouter.get('/movies/:id/shows', findShows);
-// movieRouter.get('/all', findAllMovies);
 
 module.exports = movieRouter;
